a2de8a3e41b990356ecb316254b9d57c97600279
Artir Daudi <ad28783@seeu.edu.mk>
2024-12-06 21:25:08 +0100

    Basic Exploration

---
+++ script.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+#Load the dataset
+file_path = "appleAppData.csv"
+dataset = pd.read_csv(file_path)
+
+
+#Exploring the dataset
+#print(dataset.head()) #Display the first few rows of the dataset
+#print(dataset.info()) #Check the column names and data types
+#print(dataset.describe()) #Get a summary of numerical columns
+#print(dataset.shape) #Number of rows and columns
+
